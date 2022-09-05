7927e9201eadce092e55e95be807565a68a31d38
Aranzu <se.riquelme.a@gmail.com>
2022-09-04 21:33:16 -0300

    agrega traduccion errores

--- lang/es/validation.php
+++ lang/es/validation.php
@@ -3,6 +3,13 @@
 return[
     'required' => 'El campo :attribute es obligatorio',
     'unique' => 'Ese :attribute ya está en uso',
+    'min' => [
+        'array' => 'El :attribute debe tener al menos :min elemento.',
+        'file' => 'El :attribute debe tener al menos :min kilobytes.',
+        'numeric' => 'El :attribute debe tener al menos :min.',
+        'string' => 'El :attribute debe tener al menos :min caracteres.',
+    ], 
+    'same' => 'El :attribute y :other no coinciden.',
 
     'attributes' => [
         'name' => 'nombre',
