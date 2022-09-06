803a4eb295d3353c6ffb5d0676f08a25e2f12a39
Empowerdeith <clone10700@hotmail.com>
2022-09-05 21:45:06 -0400

    remove some useless stuff

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
-use Alert;
 
 class LoginController extends Controller
 {
