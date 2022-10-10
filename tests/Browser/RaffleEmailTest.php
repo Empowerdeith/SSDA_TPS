db12cbde85e3f0d8bcffed7024092465a60d73ff
Aranzu <se.riquelme.a@gmail.com>
2022-10-10 13:25:49 -0300

    unit test, dusk test

---
+++ tests/Browser/RaffleEmailTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RaffleEmailTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_send_email()
+    //necesariamente hay que revisar la casilla de correo del destinatario para revisar si le llego el correo
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('username', 'Admin')
+                    ->type('password', '12345678')
+                    ->press('Acceder')
+                    ->visit('/raffle_auto')
+                    ->press('Realizar Sorteo')
+                    ->press('Guardar y Enviar Sorteo')
+                    ->press('Enviar')
+                    ->assertSee('OK');
+        });
+    }
+}
