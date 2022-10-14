e94aba1efb3aa9f5c89ad11ef10e65471b27a928
Empowerdeith <clone10700@hotmail.com>
2022-10-14 07:17:14 -0300

    traducción a mensajes de laravel, mejoras mensajes de alerta, incorporación página error 404, mejoras a sorteo manual

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+      <meta charset="utf-8">
+      <title>Página no encontrada</title>
+      <link rel="stylesheet" href="{{ asset('css/error404.css')}}">
+   </head>
+   <body>
+      <div id="error-page" style="border-radius: 32px;">
+         <div class="content">
+            <h2 class="header">
+               404
+            </h2>
+            <h4>
+               Ups! La página no existe.
+            </h4>
+            <p>
+               Lo sentimos, la página que estas buscando no existe, si piensas que esto es un error contacta con él/los administrador/es del sitio.
+            </p>
+            <div class="btns">
+               <a href="/">Volver al Inicio</a>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
