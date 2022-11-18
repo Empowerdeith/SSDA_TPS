024a673c1955e9b3b6670e1cdf63b6c013bb4a36
Empowerdeith <clone10700@hotmail.com>
2022-11-18 08:15:17 -0300

    mejoras a todo el proceso de enviar emails

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+      <meta charset="utf-8">
+      <title>Página no encontrada</title>
+      <link rel="stylesheet" href="{{ asset('css/error404.css')}}">
+   </head>
+   <body>
+      <div id="error-page" style="border-radius: 32px;">
+         <div class="content">
+            <h4>
+               Tu sesión ha expirado.
+            </h4>
+            <p>
+               Has estado inactivo por un largo período, por esta razón te hemos desconectado de tu cuenta.<br><br>
+               Para seguir usando el Sistema de Test de Sorteo de Drogas y Alcohol, debes iniciar sesión.
+            </p>
+            <div class="btns">
+               <a href="/">Volver al Inicio</a>
+            </div>
+            <div class="btns">
+                <a href="/login">Iniciar Sesión</a>
+             </div>
+         </div>
+      </div>
+   </body>
+</html>
